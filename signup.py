ddb0471af025fa01b92d41f4ed77cd7b79a2b1be
hangbozhang <59976512+hangbozhang@users.noreply.github.com>
2023-03-27 01:51:45 -1000

    simplify signup code

--- signup.py
+++ signup.py
@@ -23,57 +23,32 @@ def check_password(password):
 class Signup(QWidget):
 	def __init__(self):
 		super().__init__()
-		self.setWindowTitle("SignUp")
+		self.setWindowTitle("Signup")
 		self.setMinimumSize(512, 512)
 
 		# create a database instance
 		self.db = Database()
 
-		username = QLabel("Username:")
-		self.user_edit = QLineEdit()
+		# create the labels and line edits
+		self.fields = {
+			"Username": QLineEdit(),
+			"Password": QLineEdit(),
+			"Retype Password": QLineEdit(),
+			"First Name": QLineEdit(),
+			"Last Name": QLineEdit(),
+			"Phone Number": QLineEdit(),
+		}
 
-		password = QLabel("Password:")
-		self.pass_edit = QLineEdit()
-		self.pass_edit.setEchoMode(QLineEdit.Password)
+		# set the password fields to be in password mode
+		self.fields["Password"].setEchoMode(QLineEdit.Password)
+		self.fields["Retype Password"].setEchoMode(QLineEdit.Password)
 
-		password2 = QLabel("Retype Password:")
-		self.pass_edit2 = QLineEdit()
-		self.pass_edit2.setEchoMode(QLineEdit.Password)
-
-		firstname = QLabel("Fist Name:")
-		self.firstname_edit = QLineEdit()
-
-		lastname = QLabel("Last Name:")
-		self.lastname_edit = QLineEdit()
-
-		phone = QLabel("Phone Number:")
-		self.phone_edit = QLineEdit()
-
+		# create submit and cancel buttons
 		submit_button = QPushButton("Submit")
 		cancel_button = QPushButton("Cancel")
 
-		user_layout = QHBoxLayout()
-		user_layout.addWidget(username)
-		user_layout.addWidget(self.user_edit)
-		pass_layout = QHBoxLayout()
-		pass_layout.addWidget(password)
-		pass_layout.addWidget(self.pass_edit)
-		pass2_layout = QHBoxLayout()
-		pass2_layout.addWidget(password2)
-		pass2_layout.addWidget(self.pass_edit2)
-		firstname_layout = QHBoxLayout()
-		firstname_layout.addWidget(firstname)
-		firstname_layout.addWidget(self.firstname_edit)
-		lastname_layout = QHBoxLayout()
-		lastname_layout.addWidget(lastname)
-		lastname_layout.addWidget(self.lastname_edit)
-		phone_layout = QHBoxLayout()
-		phone_layout.addWidget(phone)
-		phone_layout.addWidget(self.phone_edit)
-		button_layout = QHBoxLayout()
-		button_layout.addWidget(submit_button)
-		button_layout.addWidget(cancel_button)
-
+		# create the layouts and add widgets to them
+		v_layout = QVBoxLayout()
 		message_layout = QHBoxLayout()
 		self.message_label = QLabel()
 		self.message_label.setStyleSheet('color: red')
@@ -82,29 +57,40 @@ class Signup(QWidget):
 		self.message_label.setWordWrap(True)
 		message_layout.addWidget(self.message_label)
 
-		v_layout = QVBoxLayout()
-		v_layout.addLayout(user_layout)
-		v_layout.addLayout(pass_layout)
-		v_layout.addLayout(pass2_layout)
-		v_layout.addLayout(firstname_layout)
-		v_layout.addLayout(lastname_layout)
-		v_layout.addLayout(phone_layout)
+		for field, widget in self.fields.items():
+			layout = QHBoxLayout()
+			label = QLabel(field)
+			layout.addWidget(label)
+			layout.addWidget(widget)
+			v_layout.addLayout(layout)
+
 		v_layout.addLayout(message_layout)
+		# add submit and cancel buttons to a layout
+		button_layout = QHBoxLayout()
+		button_layout.addWidget(submit_button)
+		button_layout.addWidget(cancel_button)
 		v_layout.addLayout(button_layout)
 
+		# set the main layout of the window
 		self.setLayout(v_layout)
 
+		# connect submit and cancel buttons to their respective functions
 		submit_button.clicked.connect(self.submit)
 		cancel_button.clicked.connect(self.close)
 
 	def submit(self):
-		self.message_label.setText("")
-		username = self.user_edit.text()
-		password = self.pass_edit.text()
-		re_password = self.pass_edit2.text()
-		firstname = self.firstname_edit.text()
-		lastname = self.lastname_edit.text()
-		phone = self.phone_edit.text()
+		# get the input values from the fields
+		username = self.fields["Username"].text()
+		password = self.fields["Password"].text()
+		retype_password = self.fields["Retype Password"].text()
+		first_name = self.fields["First Name"].text()
+		last_name = self.fields["Last Name"].text()
+		phone_number = self.fields["Phone Number"].text()
+
+		# check if any fields are empty
+		if not all((username, password, retype_password, first_name, last_name, phone_number)):
+			self.message_label.setText("Please fill in all fields.")
+			return
 
 		if not check_password(password):
 			self.message_label.setText(
@@ -112,20 +98,23 @@ class Signup(QWidget):
 				"with combination of letters, numbers and special characters, "
 				"and at least 1 capitalized letter."
 			)
-		else:
-			if password != re_password:
-				self.message_label.setText("The retyped password does not match")
-			else:
-				if not phone.isdigit():
-					self.message_label.setText("Please input only numbers for phone number")
-				elif len(phone) != 10:
-					self.message_label.setText("Please only enter 10 numbers for phone number")
-				else:
-					self.db.insert_user(username, password, firstname, lastname, phone)
-					self.message_label.setText(f"Signed up correctly for user {username}")
-					self.user_edit.clear()
-					self.pass_edit.clear()
-					self.pass_edit2.clear()
-					self.firstname_edit.clear()
-					self.lastname_edit.clear()
-					self.phone_edit.clear()
+			return
+
+		# check if passwords match
+		if password != retype_password:
+			self.message_label.setText("Passwords do not match.")
+			return
+
+		# validate phone number
+		if not re.match(r'^\d{10}$', phone_number):
+			self.message_label.setText("Invalid phone number. Must be 10 digits.")
+			return
+
+		# add the user to the database
+		self.db.insert_user(username, password, first_name, last_name, phone_number)
+
+		# show success message
+		self.message_label.setStyleSheet('color: green')
+		self.message_label.setText(f"Signed up correctly for user {username}")
+		for field in self.fields.values():
+			field.clear()
